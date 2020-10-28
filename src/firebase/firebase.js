b2d0a6971c6604ea99b86fb77e263c17d543763d
Wajdi <wajdidouiri26@gmail.com>
2020-10-28 17:19:20 +0100

    heroku coudit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCrk3__tKfoFoN1iyyWPZ3-w22wGWq9JHA",
-    authDomain: "expensify-9a53e.firebaseapp.com",
-    databaseURL: "https://expensify-9a53e.firebaseio.com",
-    projectId: "expensify-9a53e",
-    storageBucket: "expensify-9a53e.appspot.com",
-    messagingSenderId: "2815567488",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: "1:2815567488:web:ae94ef42906fe5226ae865",
     measurementId: "G-DPEKY0KFQG"
 };
