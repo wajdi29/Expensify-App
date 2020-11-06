e9a23871c7480a87dbb624ddd3287bfb7bf731b1
Wajdi <wajdidouiri26@gmail.com>
2020-11-06 17:35:56 +0100

    add final styles

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const LoadingPage = () => (
+    <div className="loader">
+        <img src="/img/loader.gif" className="loader__img" />
+    </div>
+
+)
+
+
+export default LoadingPage
